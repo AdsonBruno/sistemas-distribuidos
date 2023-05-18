4f0ee5b12497f23b6499361637631bd27addf62b
AdsonBruno <adsonbruno2@gmail.com>
2023-05-18 11:33:05 -0300

    feat: add create new thread

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from lock import Lock
+import threading
+
+def worker(lock):
+  lock.acquire()
+  locked_by = lock.locked_by()
+  print('Bloqueio adquirido pela thread: ',  locked_by)
+  lock.release()
 
 def main():
   lock = Lock()
 
+  thread = threading.Thread(target=worker, args=(lock,))
+  thread.start()
+
+  thread.join()
+  
   lock.acquire()
 
   locked_by = lock.locked_by()
