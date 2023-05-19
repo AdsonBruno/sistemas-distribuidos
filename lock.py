fe34ddd39750a678b145d497579f911846250815
AdsonBruno <adsonbruno2@gmail.com>
2023-05-19 00:02:58 -0300

    feat: add  modifications to manage access requests with a list in lock.py

--- lock.py
+++ lock.py
@@ -5,17 +5,22 @@ class Lock:
     self._lock = threading.Lock()
     self._locked = False
     self._locked_by = None
+    self._request_queue = []
 
   def acquire(self): 
     self._lock.acquire()
-    while self._locked:
+    self._request_queue.append(threading.current_thread().name)
+    while self._locked or self._request_queue[0] != threading.current_thread().name:
       self._lock.release()
       self._lock.acquire()
     
     self._locked = True
     self._locked_by = threading.current_thread().name
+    self._lock.release()
 
   def release(self):
+    self._lock.acquire()
+    self._request_queue.pop(0)
     self._locked = False
     self._lock.release()
 
